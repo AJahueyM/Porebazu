73390b3e3989890ed575209188fbb557dd9c48cc
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2026-07-05 21:52:29 -0700

    Update local deploy script

--- infra/local-deploy.ps1
+++ infra/local-deploy.ps1
@@ -3,7 +3,15 @@ param(
     [string]$deployLocation = "westus2"
 )
 
-az deployment sub create --name "infra-deploy" --location "$deployLocation" --template-file "main.bicep" --parameters "params/$env.bicepparam"
+$sshKeyPath = "~\.ssh\id_rsa.pub"
+if (-not (Test-Path $sshKeyPath)) {
+    Write-Error "SSH public key not found at $sshKeyPath. Please generate an SSH key pair and place the public key at this location."
+    exit 1
+}
+
+sshPublicKey = Get-Content $sshKeyPath -Raw
+
+az deployment sub create --name "infra-deploy" --location "$deployLocation" --template-file "main.bicep" --parameters "params/$env.bicepparam" --parameters adminPublicKey="$sshPublicKey"
 
 if ($LASTEXITCODE -ne 0) {
     Write-Error "Deployment failed with exit code $LASTEXITCODE"
